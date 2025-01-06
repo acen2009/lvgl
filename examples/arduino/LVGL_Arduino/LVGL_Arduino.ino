0ba366f85f1c5a38c5ec9b47ce097082824693f0
acen2009 <acen2005@gmail.com>
2025-01-06 16:07:43 +0800

    modify the Arduino example for vortex86

--- examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
+++ examples/arduino/LVGL_Arduino/LVGL_Arduino.ino
@@ -13,11 +13,11 @@
  *as the examples and demos are now part of the main LVGL library. */
 
 //#include <examples/lv_examples.h>
-//#include <demos/lv_demos.h>
+#include <demos/lv_demos.h>
 
 /*Set to your screen resolution and rotation*/
-#define TFT_HOR_RES   320
-#define TFT_VER_RES   240
+#define TFT_HOR_RES   1024
+#define TFT_VER_RES   600
 #define TFT_ROTATION  LV_DISPLAY_ROTATION_0
 
 /*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
@@ -100,15 +100,14 @@ void setup()
 
 #else
     /*Else create a display yourself*/
-    disp = lv_display_create(TFT_HOR_RES, TFT_VER_RES);
-    lv_display_set_flush_cb(disp, my_disp_flush);
-    lv_display_set_buffers(disp, draw_buf, NULL, sizeof(draw_buf), LV_DISPLAY_RENDER_MODE_PARTIAL);
+    disp = lv_vortex86_vga_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, sizeof(draw_buf));
+    lv_vortex86_inputs_create(disp, LV_VORTEX86_INPUT_INCLUDE_KBMS, NULL);
 #endif
 
     /*Initialize the (dummy) input device driver*/
-    lv_indev_t * indev = lv_indev_create();
-    lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
-    lv_indev_set_read_cb(indev, my_touchpad_read);
+    // lv_indev_t * indev = lv_indev_create();
+    // lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER); /*Touchpad should have POINTER type*/
+    // lv_indev_set_read_cb(indev, my_touchpad_read);
 
     /* Create a simple label
      * ---------------------
@@ -128,12 +127,14 @@ void setup()
 
      lv_demo_widgets();
      */
+     
+    lv_demo_widgets();
 
-    lv_obj_t *label = lv_label_create( lv_screen_active() );
-    lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
-
-    Serial.println( "Setup done" );
+    // lv_obj_t *label = lv_label_create( lv_screen_active() );
+    // lv_label_set_text( label, "Hello Arduino, I'm LVGL!" );
+    // lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+    
+    // Serial.println( "Setup done" );
 }
 
 void loop()
